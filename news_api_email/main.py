3a43a4a3ce6742b0e8d1006bb37c95233ce9eecb
arturexp <arturexp@gmail.com>
2023-02-09 14:04:11 +0700

    added links, language, topics for news

--- news_api_email/main.py
+++ news_api_email/main.py
@@ -2,20 +2,24 @@ import requests
 from send_email import send_email
 
 
-link = "https://newsapi.org/v2/"
+link = "https://newsapi.org/v2/everything?"
 api_key = "apiKey=239e97455d7e4c8a9f956349d2f46e13"
-d = "everything?q=tesla&from=2023-01-09&sortBy=publishedAt&"
-url = f"{link}{d}{api_key}"
+topic = "q=tesla"
+d = f"&from=2023-01-09&sortBy=publishedAt&"
+lang = "&language=en"
+url = f"{link}{topic}{d}{api_key}{lang}"
 
 request = requests.get(url)
 content = request.json()
 
 body = ""
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + \
-            article["description"] + 2 * "\n"
+        body = "Subject: Today's news" "\n" + \
+            body + article["title"] + "\n" + \
+            article["description"] + "\n" + \
+            article["url"] + 2 * "\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
