a14cc27155ef99420381803f0d65c125ced8ee81
arturexp <arturexp@gmail.com>
2023-02-13 22:03:25 +0700

    slug

--- Django/try_django/articles/models.py
+++ Django/try_django/articles/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -14,3 +16,8 @@ class Article(models.Model):
                                default=timezone.now,
                                null=True,
                                blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*artgs, **kwargs)
