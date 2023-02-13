f295a6f577eb0e359385f5e35b94d5ab096356c0
arturexp <arturexp@gmail.com>
2023-02-13 09:42:36 +0700

    register form

--- Django/try_django/trydjango/urls.py
+++ Django/try_django/trydjango/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from .views import home_view
 from articles.views import (article_search_view, article_create_view,
                             article_detail_view)
-from accounts.views import (login_view, logout_view)
+from accounts.views import (login_view, logout_view, register_view)
 
 urlpatterns = [
     path('', home_view),  # index home root
@@ -28,4 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view),
     path('logout/', logout_view),
+    path('register/', register_view),
 ]
