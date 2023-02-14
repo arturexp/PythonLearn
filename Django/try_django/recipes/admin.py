506458ec9f70da36f6ed1bdc2028d2564d9a9522
arturexp <arturexp@gmail.com>
2023-02-14 22:34:07 +0700

    admin inlines

--- Django/try_django/recipes/admin.py
+++ Django/try_django/recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import RecipeIngredient, Recipe
+
+#admin.site.register(RecipeIngredient)
+
+
+class RecipeIngridientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    #fields = ['name', 'quantity', 'unit', 'directions']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngridientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+
+admin.site.register(Recipe, RecipeAdmin)
