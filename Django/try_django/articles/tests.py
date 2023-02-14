352afec273a258b8cb080556d7bc184a5349cd0e
arturexp <arturexp@gmail.com>
2023-02-14 14:18:15 +0700

    writing tests for Article model

--- Django/try_django/articles/tests.py
+++ Django/try_django/articles/tests.py
@@ -3,12 +3,13 @@ from django.test import TestCase
 # Create your tests here.
 from .models import Article
 from django.utils.text import slugify
+from .utils import slugify_instance_title
 
 
 class ArticleTestCase(TestCase):
 
     def setUp(self):
-        self.number_of_articles = 5
+        self.number_of_articles = 500
         for i in range(0, self.number_of_articles):
             # db for tests
             Article.objects.create(title='Hello world',
@@ -37,3 +38,18 @@ class ArticleTestCase(TestCase):
             slug = obj.slug
             slugified_title = slugify(title)
             self.assertNotEqual(slug, slugified_title)
+
+    def test_slugify_instance_title(self):
+        obj = Article.objects.all().last()
+        new_slugs = []
+        for i in range(0, 25):
+            instance = slugify_instance_title(obj, save=False)
+            new_slugs.append(instance.slug)
+
+        unique_slugs = list(set(new_slugs))
+        self.assertEqual(len(new_slugs), len(unique_slugs))
+
+    def test_slugify_instance_title_redux(self):
+        slug_list = Article.objects.all().values_list('slug', flat=True)
+        unique_slug_list = list(set(slug_list))
+        self.assertEqual(len(slug_list), len(unique_slug_list))
