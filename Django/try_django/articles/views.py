8d7b792745dac5856e6832292de80fea4340c170
arturexp <arturexp@gmail.com>
2023-02-14 15:16:37 +0700

    slug dynamic urls

--- Django/try_django/articles/views.py
+++ Django/try_django/articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from articles.models import Article
 from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
+from django.http import Http404
 
 
 # Create your views here.
@@ -54,10 +55,17 @@ def article_create_view(request):
 #     return render(request, "articles/create.html", context=context)
 
 
-def article_detail_view(request, id=None):
+def article_detail_view(request, slug=None):
     article_obj = None
-    if id is not None:
-        article_obj = Article.objects.get(id=id)
+    if slug is not None:
+        try:
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
     context = {
         "object": article_obj,
     }
