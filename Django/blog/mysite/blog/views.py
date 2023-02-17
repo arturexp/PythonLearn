eb6dc679f7fd3452868ce8a123920564e99a62dc
arturexp <arturexp@gmail.com>
2023-02-17 10:07:11 +0700

    comment system  for posts

--- Django/blog/mysite/blog/views.py
+++ Django/blog/mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 
@@ -59,4 +60,29 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # list of active comments for this post
+    comments = post.comments.filter(active=True)
+    # form for users to comment
+    form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a comment object without saving it to the db
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the db
+        comment.save()
+    return render(request,
+                  'blog/post/comment.html',
+                  {'post': post, 'form': form, 'comment': comment})
