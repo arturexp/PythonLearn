3bec40e55d67dd8a2f4956beaf2d14a54c49346e
arturexp <arturexp@gmail.com>
2023-02-21 21:57:54 +0700

    drf serializer

---
+++ Django/drf/backend/api/views.py
@@ -0,0 +1,17 @@
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
+
+
+@api_view(["GET", "POST"])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        data = ProductSerializer(instance).data
+    return Response(data)
