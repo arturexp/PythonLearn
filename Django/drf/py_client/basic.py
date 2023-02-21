525cbfad35202dfa087bd0231b796b774084ac7b
arturexp <arturexp@gmail.com>
2023-02-21 20:07:23 +0700

    simple api with django

--- Django/drf/py_client/basic.py
+++ Django/drf/py_client/basic.py
@@ -1,5 +1,9 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
 
-requests.get()
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint,
+                            params={"abc": 123},
+                            json={"query": "Hello world"})  # HTTP request
+print(get_response.json())
