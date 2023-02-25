73216a00112538cf78c22a3d32ddcb82a292c3aa
arturexp <arturexp@gmail.com>
2023-02-25 21:28:49 +0700

    templates, views, context

--- Django/django_crm/leads/views.py
+++ Django/django_crm/leads/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_page(request):
+    # return HttpResponse('Hello world')
+    context = {
+        'name': 'Joe',
+        'age': 35
+    }
+    return render(request, 'second_page.html', context)
