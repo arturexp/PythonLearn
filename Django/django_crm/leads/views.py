a0701f48a1094937e53d59ff7e7c9d47259b02a5
arturexp <arturexp@gmail.com>
2023-02-25 22:30:21 +0700

    tamplates for all leads and detail view

--- Django/django_crm/leads/views.py
+++ Django/django_crm/leads/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 
 
-def home_page(request):
-    # return HttpResponse('Hello world')
+def lead_list(request):
+    leads = Lead.objects.all()
     context = {
-        'name': 'Joe',
-        'age': 35
+        'leads': leads
     }
-    return render(request, 'second_page.html', context)
+    return render(request, 'leads/lead_list.html', context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead': lead,
+    }
+    return render(request, 'leads/lead_detail.html', context)
