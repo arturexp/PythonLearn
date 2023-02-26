994757377f66d0c891b21fdfa6f7c49a8cd87543
arturexp <arturexp@gmail.com>
2023-02-26 18:09:47 +0700

    update view using ModelForm

--- Django/django_crm/leads/views.py
+++ Django/django_crm/leads/views.py
@@ -20,6 +20,21 @@ def lead_detail(request, pk):
     return render(request, 'leads/lead_detail.html', context)
 
 
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+    context = {
+        'form': form,
+        'lead': lead,
+    }
+    return render(request, 'leads/lead_update.html', context)
+
+
 def lead_create(request):
     form = LeadModelForm()
     if request.method == 'POST':
